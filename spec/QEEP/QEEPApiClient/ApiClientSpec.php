9360ade006ff15c13592b3dfd2c4b3653c7e1558
Igor Kamyshev <garik.novel@gmail.com>
2017-11-27 15:57:01 +0700

    add first test

--- spec/QEEP/QEEPApiClient/ApiClientSpec.php
+++ spec/QEEP/QEEPApiClient/ApiClientSpec.php
@@ -5,12 +5,17 @@ namespace spec\QEEP\QEEPApiClient;
 use QEEP\QEEPApiClient\ApiClient;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use QEEP\QEEPApiClient\Model\OrderStatus;
 
 class ApiClientSpec extends ObjectBehavior
 {
     public function it_is_initializable()
     {
-        $this->beConstructedWith(1, '', '', '', '');
-        $this->shouldHaveType(ApiClient::class);
+        $client = new ApiClient(...$this->getApiClientParams());
+    }
+
+    private function getApiClientParams()
+    {
+        return [1, 'secret', 'crm url', 'channel', 'image url'];
     }
 }
