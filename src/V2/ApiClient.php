baf52f538fe5e70c35628ce7d1502312d6d66afd
natasha <imbolc91@mail.ru>
2018-03-28 15:29:58 +0700

    added CompanyInfo and function getInfo to API Client QEEP-373

--- src/V2/ApiClient.php
+++ src/V2/ApiClient.php
@@ -6,6 +6,7 @@ namespace QEEP\QEEPApiClient\V2;
 use JMS\Serializer\SerializerBuilder;
 use QEEP\QEEPApiClient\V2\Model\Brand;
 use QEEP\QEEPApiClient\V2\Model\Category;
+use QEEP\QEEPApiClient\V2\Model\CompanyInfo;
 use QEEP\QEEPApiClient\V2\Model\CustomQuestion;
 use QEEP\QEEPApiClient\V2\Model\Option;
 use QEEP\QEEPApiClient\V2\Model\Parameter;
@@ -82,9 +83,6 @@ class ApiClient
     /** @return Product[] */
     public function getProducts() : array
     {
-        var_dump($this->callApiV2Method('products/get'));
-        var_dump('-----------');
-
         return $this->deserializeArray(
             $this->callApiV2Method('products/get'),
             Product::class
@@ -169,5 +167,14 @@ class ApiClient
             'client_id' => $this->clientId,
             'access_token' => md5($this->clientId . $this->clientSecret . $paramsString),
         ];
+    }
+
+    /** @return CompanyInfo[] */
+    private function getInfo(): array
+    {
+        return $this->deserializeArray(
+            $this->callApiV2Method('info/get'),
+            CompanyInfo::class
+        );
     }
 }
---
+++ src/V2/Model/CompanyInfo.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace QEEP\QEEPApiClient\V2\Model;
+
+use JMS\Serializer\Annotation as JMS;
+
+class CompanyInfo
+{
+    /** @JMS\Type("string") */
+    protected $key;
+
+    /** @JMS\Type("string") */
+    protected $value;
+
+    public function getKey(): ?string
+    {
+        return $this->key;
+    }
+
+    public function setId(string $key): CompanyInfo
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(?string $value): CompanyInfo
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+
+}
