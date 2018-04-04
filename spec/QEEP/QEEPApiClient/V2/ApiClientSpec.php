c21790b49c86611962d8592f598af0f983b0a48e
natasha <imbolc91@mail.ru>
2018-04-04 14:34:10 +0700

    added test fo getCities

--- spec/QEEP/QEEPApiClient/V2/ApiClientSpec.php
+++ spec/QEEP/QEEPApiClient/V2/ApiClientSpec.php
@@ -2,8 +2,10 @@
 
 namespace spec\QEEP\QEEPApiClient\V2;
 
+use phpmock\environment\MockEnvironment;
 use QEEP\QEEPApiClient\V2\ApiClient;
 use PhpSpec\ObjectBehavior;
+use phpmock\MockBuilder;
 
 class ApiClientSpec extends ObjectBehavior
 {
@@ -14,9 +16,16 @@ class ApiClientSpec extends ObjectBehavior
 
     function it_is_able_to_return_cities()
     {
-        $this->beConstructedWith(...$this->getApiClientParams());
+        $mockCurl = $this->buildMockCurl(
+            '["Томск", "Николаев"]'
+        );
 
+        $mockCurl->enable();
+
+        $this->beConstructedWith(...$this->getApiClientParams());
         $this->getCities()->shouldReturn(['Томск', 'Николаев']);
+
+        $mockCurl->disable();
     }
 
     private function getApiClientParams()
@@ -28,5 +37,42 @@ class ApiClientSpec extends ObjectBehavior
             'shop.qeep.pro',
             'https://demo.qeep.pro/catalog/',
         ];
+    }
+
+    private function buildMockCurl(string $execReturn): MockEnvironment
+    {
+        $mockEnvironment = new MockEnvironment();
+
+        $builder = new MockBuilder();
+
+        $builder->setNamespace('QEEP\QEEPApiClient\V2')
+            ->setName("curl_exec")
+            ->setFunction(
+                function ($_) use ($execReturn) {
+                    return $execReturn;
+                }
+            );
+
+        $mockExec = $builder->build();
+        $mockEnvironment->addMock($mockExec);
+
+        $builder->setNamespace('QEEP\QEEPApiClient\V2')
+            ->setName('curl_getinfo')
+            ->setFunction(function ($_) {
+                return 200;
+            });
+
+        $mockInfo = $builder->build();
+        $mockEnvironment->addMock($mockInfo);
+        $builder->setNamespace('QEEP\QEEPApiClient\V2')
+            ->setName('curl_error')
+            ->setFunction(function($_) {
+                return null;
+            });
+
+        $mockError = $builder->build();
+        $mockEnvironment->addMock($mockError);
+
+        return $mockEnvironment;
     }
 }
