8c870bd83d321347dd9002516a75b8f27472b258
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2018-06-27 12:16:00 +0700

    add positions for parameters

--- src/V2/Model/Parameter.php
+++ src/V2/Model/Parameter.php
@@ -12,6 +12,9 @@ class Parameter
     /** @JMS\Type("string") */
     protected $name;
 
+    /** @JMS\Type("integer") */
+    protected $position;
+
     /** @JMS\Type("string") */
     protected $characteristic;
 
@@ -35,6 +38,18 @@ class Parameter
     public function setName(?string $name): Parameter
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPosition(): ?int
+    {
+        return $this->position;
+    }
+
+    public function setPosition(?int $position): Parameter
+    {
+        $this->position = $position;
 
         return $this;
     }
